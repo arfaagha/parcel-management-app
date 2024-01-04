44d3950caad434e459dc6382b9a162f9c14f598c
AD0380 <arfa.saif92@gmail.com>
2024-01-04 03:24:43 +0200

    refactor add-parcel component

--- src/app/add-parcel/add-parcel.component.ts
+++ src/app/add-parcel/add-parcel.component.ts
@@ -1,28 +1,88 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ParcelService } from "../parcel.service";
+import { Parcel } from "../parcel";
 
 @Component({
-  selector: 'app-add-parcel',
-  templateUrl: './add-parcel.component.html',
-  styleUrl: './add-parcel.component.css'
+  selector: "app-add-parcel",
+  templateUrl: "./add-parcel.component.html",
+  styleUrl: "./add-parcel.component.css",
 })
 export class AddParcelComponent {
+  submitButton() {
+    this.createParcel(this.createParcelForm.value as Parcel);
 
-submitButton() {
-console.log()
-}
+    // TODO: get http response from API and
+    // display appropriate message along with resetting the form
+
+    this.createParcelForm.reset();
+  }
 
   createParcelForm!: FormGroup;
   created = false;
 
-  constructor(private formBuilder: FormBuilder){
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: ParcelService
+  ) {
     this.createParcelForm = this.formBuilder.group({
-      sku: this.formBuilder.control('', Validators.compose([Validators.required, Validators.pattern(new RegExp(/^\d{4}-\d{8}-\d{4}$/gm))])),
-      description: this.formBuilder.control('', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(200)])),
-      streetAddress: this.formBuilder.control('', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(50)])),
-      town: this.formBuilder.control('', Validators.compose([Validators.required,Validators.minLength(2), Validators.maxLength(40)])),
-      country: this.formBuilder.control('', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(20)])),
-      deliveryDate: this.formBuilder.control(new Date(), Validators.compose([Validators.required])),
-    })
+      sku: this.formBuilder.control(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(new RegExp(/^\d{4}-\d{8}-\d{4}$/gm)),
+        ])
+      ),
+      description: this.formBuilder.control(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(200),
+        ])
+      ),
+      streetAddress: this.formBuilder.control(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(50),
+        ])
+      ),
+      town: this.formBuilder.control(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40),
+        ])
+      ),
+      country: this.formBuilder.control(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(20),
+        ])
+      ),
+      deliveryDate: this.formBuilder.control(
+        new Date(),
+        Validators.compose([Validators.required])
+      ),
+    });
+  }
+
+  createParcel(parcel: Parcel) {
+    this.service.createParcel(parcel).subscribe((data) => {});
+  }
+
+  checkSkuValidity() {
+    this.service
+      .checkSkuValidity(this.createParcelForm.value.sku)
+      .subscribe((data) => {
+        if (!data) {
+          this.createParcelForm.controls["sku"].setErrors({ incorrect: true });
+        }
+      });
   }
 }
--- src/app/parcel.service.ts
+++ src/app/parcel.service.ts
@@ -1,34 +1,41 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import {Parcel} from './parcel';
-import { Observable, catchError, of } from 'rxjs';
-import { environment } from '../environment/environment';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Parcel } from "./parcel";
+import { Observable, catchError, of } from "rxjs";
+import { environment } from "../environment/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-
 export class ParcelService {
+  baseUrl = environment.apiUrl;
+  parcels: Parcel[] = [];
+  constructor(private http: HttpClient) {}
 
-  baseUrl= environment.apiUrl;
-  parcels: Parcel[]=[];
-  constructor(private  http: HttpClient) { 
+  getAllParcels(): Observable<Object | Parcel[]> {
+    // TODO: Searching and filtering can be done using the same call
 
+    return this.http
+      .get(this.baseUrl + "/parcels")
+      .pipe(catchError(this.handleError<Parcel[]>("getAllParcels", [])));
   }
 
-  getAllParcels(): Observable<Object | Parcel[]>{
-    return this.http.get(this.baseUrl+ '/parcels').pipe(
-      catchError(this.handleError<Parcel[]>('getAllParcels', [])));
+  createParcel(parcel: Parcel) {
+    return this.http.post(this.baseUrl + "/parcels", { parcel });
   }
 
-  private handleError<T>(operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
 
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
-
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-}
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  checkSkuValidity(sku: string): Observable<Object | boolean> {
+    return this.http
+      .get(this.baseUrl + "/parcels/" + sku.trim())
+      .pipe(catchError(this.handleError<boolean>("checkSkuValidity", false)));
+  }
 }
